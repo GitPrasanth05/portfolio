0302aeca858489fead18b9cd29e176a2da572479
GitPrasanth05 <155186304+GitPrasanth05@users.noreply.github.com>
2025-12-12 11:46:31 +0530

    dock done

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -4,10 +4,25 @@ import {Tooltip} from "react-tooltip";
 import {dockApps} from "#constants/index.js";
 import {useGSAP} from "@gsap/react";
 import gsap from "gsap";
+import useWindowStore from "#store/window.js";
 const Dock = () => {
+    const {openWindow , closeWindow,windows} = useWindowStore();
     const dockRef = useRef(null);
     const toggleApp=(app)=>{
     //open window
+
+        if(!app.canOpen)return;
+        const window = windows[app.id];
+        if(!window){
+         console.error("Could not open window");
+        }
+        if(window.isOpen){
+            closeWindow(app.id);
+        }
+        else{
+            openWindow(app.id);
+        }
+        console.log(windows);
     }
 
     useGSAP(()=>{
